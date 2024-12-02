312bc6ba4cc625898c802f486b9dc5778207470b
shaojing <>
2024-12-02 14:07:02 +0800

    实现Omit

---
+++ src/02.实现Omit.ts
@@ -0,0 +1,19 @@
+// Implement the built-in Omit<T, K> generic without using it.
+// Constructs a type by picking all properties from T and then removing K
+
+type MyOmit<T, K extends keyof T> = {
+  [P in Exclude<keyof T, K>]: T[P];
+};
+
+// For example
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+};
+
+type TodoPreview = MyOmit<Todo, 'description' | 'title'>;
+
+const todo: TodoPreview = {
+  completed: false,
+};
