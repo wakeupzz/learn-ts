62071cf4480a56835d1432ba682b03418bbd90cf
shaojing <>
2024-12-03 09:22:28 +0800

    fix: MyOmiy使用自定义Exclude

--- src/02.实现Omit.ts
+++ src/02.实现Omit.ts
@@ -1,8 +1,10 @@
 // Implement the built-in Omit<T, K> generic without using it.
 // Constructs a type by picking all properties from T and then removing K
 
+type MyExclude<T, K> = T extends K ? never : T;
+
 type MyOmit<T, K extends keyof T> = {
-  [P in Exclude<keyof T, K>]: T[P];
+  [P in MyExclude<keyof T, K>]: T[P];
 };
 
 // For example
