33308bc21560bb415d245d727ef9b7dac94a78fb
shaojing <shaojing@MacBook-Pro.local>
2024-12-10 09:56:15 +0800

    Implement SimpleVue

---
+++ src/06.SimpleVue.ts
@@ -0,0 +1,40 @@
+// Ref: https://github.com/type-challenges/type-challenges/blob/main/questions/00006-hard-simple-vue/README.md
+
+declare function SimpleVue<
+  D extends Record<string, unknown>,
+  C extends Record<string, unknown>,
+  M extends Record<string, unknown>,
+>(options: {
+  data: (this: never) => D;
+  computed: {
+    [K in keyof C]: (this: D, ...args: unknown[]) => C[K];
+  };
+  methods: {
+    [K in keyof M]: (
+      this: D & C & { [K in keyof M]: (...args: unknown[]) => M[K] },
+      ...args: unknown[]
+    ) => M[K];
+  };
+}): unknown;
+
+const instance = SimpleVue({
+  data() {
+    return {
+      firstname: 'Type',
+      lastname: 'Challenges',
+      amount: 10,
+    }
+  },
+  computed: {
+    fullname() {
+      return this.firstname + ' ' + this.lastname
+    }
+  },
+  methods: {
+    hi() {
+      alert(this.fullname.toLowerCase())
+    }
+  }
+})
+
+
